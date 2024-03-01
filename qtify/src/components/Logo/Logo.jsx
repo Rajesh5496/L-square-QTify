21ec91ae70639922c443bf1a765aaedefe208204
Rajeshvbridge <rajesh.r@vbridgehub.com>
2024-03-01 16:09:07 +0530

    font

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import LogoImage from "../../assets/qtifylogo.png"
+import styles from "./Logo.module.css"
 
 export default function Logo(){
-    return <img src={LogoImage} alt="Qtify Logo" width={67} height={34} />
+    return <img src={LogoImage} className={styles.center} alt="Qtify Logo" width={67} height={34} />
 }
