9645e879e5992b67f02b1cf7551461bc1316c475
Rajeshvbridge <rajesh.r@vbridgehub.com>
2024-03-01 18:34:18 +0530

    font

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import styles from "./Navbar.module.css"
+import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
 import SearchBar from "../SearchBar/SearchBar";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
+import Hero from "../Hero/Hero";
 
-function Navbar(){
-    return(
-        <nav className={styles.navbarContainer}>
-            <Logo />
-            <SearchBar />
-            <Button />
-
-        </nav>
-    )
+function Navbar() {
+  return (
+    <>
+      <nav className={styles.navbarContainer}>
+        <Logo />
+        <SearchBar />
+        <Button />
+      </nav>
+      <Hero />
+    </>
+  );
 }
 
 export default Navbar;
